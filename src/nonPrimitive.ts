57dd4b016335509dc46854750d8b58870943caa1
abujaher5 <abujaher5120@gmail.com>
2025-11-09 20:36:36 +0600

    explore nonPrimitive object type

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -10,3 +10,42 @@ mixedArr.push(14); // no error occur
 let nameAndRoll: [string, number] = ["Mirza", 120];
 
 let destination: [string, string, number] = ["Dhaka", "Chattogram", 4];
+
+// reference type : object
+const user: {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+} = {
+  firstName: "Abu",
+  middleName: "Jaher",
+  lastName: "Rasel",
+};
+const user1: {
+  firstName: string;
+  middleName?: string; // ? means middleName is optional.
+  lastName: string;
+  isStudent: boolean;
+  organization: "Programming Hero"; // "Programming Hero" is a value type : literal types
+} = {
+  organization: "Programming Hero",
+  firstName: "Abu",
+  lastName: "Jaher",
+  isStudent: true,
+};
+// user1.organization="Next Level Web Dev"  // error occur => literal type
+
+const user2: {
+  firstName: string;
+  middleName?: string; // ? means middleName is optional.
+  lastName: string;
+  isStudent: boolean;
+  readonly organization: string; // readonly => access modifier immutable
+} = {
+  organization: "Programming Hero",
+  firstName: "Abu",
+  lastName: "Jaher",
+  isStudent: true,
+};
+
+// user2.organization = "Programming Hero"; // error occur cause readonly access modifier is immutable
