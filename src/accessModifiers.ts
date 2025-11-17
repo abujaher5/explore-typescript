443eccdea7c3c8067621ec3a18075a6e244a8684
abujaher5 <abujaher5120@gmail.com>
2025-11-17 20:38:38 +0600

    explore accessModifiers

---
+++ src/accessModifiers.ts
@@ -0,0 +1,27 @@
+// access modifiers
+
+class BankAccount {
+  readonly userId: number;
+  public userName: string;
+  protected userBalance: number;
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
+
+  addBalance(balance: number) {
+    this.userBalance = this.userBalance + balance;
+  }
+}
+
+const userAccount = new BankAccount(11, "User Name", 20);
+userAccount.addBalance(200);
+userAccount.addBalance(80);
+// console.log(userAccount);
+
+class StudentBankAccount extends BankAccount {
+  test() {
+    this.userBalance; // can get the access by using protected modifiers
+  }
+}
